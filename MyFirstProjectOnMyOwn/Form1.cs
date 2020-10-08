17d54bd2461f6a29cf0511d6286a06e0ad583bcb
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2020-10-08 18:21:19 +0200

    First commit of the program.
    I've created the solution, added a button on the second window and added a cancel button.

---
+++ MyFirstProjectOnMyOwn/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstProjectOnMyOwn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Keep calm Thanos...", "MiMensaje", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+                Environment.Exit(0);
+        }
+    }
+}
